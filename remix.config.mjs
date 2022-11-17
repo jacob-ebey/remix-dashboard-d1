dba06b8159095f6ac123ea6be5ef5be5335bdc86
Jacob Ebey <jacob.ebey@live.com>
2022-11-16 21:15:44 -0800

    add dev delay

--- remix.config.mjs
+++ remix.config.mjs
@@ -3,6 +3,7 @@ import { flatRoutes } from "remix-flat-routes";
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
 	serverModuleFormat: "esm",
+	devServerBroadcastDelay: 1000,
 	ignoredRouteFiles: ["**/*"],
 	routes: async (defineRoutes) => {
 		return flatRoutes("routes", defineRoutes, {
